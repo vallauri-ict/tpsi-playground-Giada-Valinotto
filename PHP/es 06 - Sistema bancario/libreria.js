d979eff3152df8ffd68d824d6aa6acf18573f02b
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-05-23 12:15:00 +0200

    Esercizio 06 banche completato

--- PHP/es 06 - Sistema bancario/libreria.js
+++ PHP/es 06 - Sistema bancario/libreria.js
@@ -1,31 +1,44 @@
 "use strict";
 
-const URL = "http://localhost"
+const PHP = true;
 
 function inviaRichiesta(method, url, parameters={}) {
-	let contentType;
-	if(method.toUpperCase()=="GET")
+    let contentType;
+    
+	// urlencoded --> formato nome = value
+	if(method.toUpperCase()=="GET" || PHP)
 		contentType="application/x-www-form-urlencoded;charset=utf-8";
 	else{
+		// se la chiamata è post serializza i dati e li invia
 		contentType = "application/json; charset=utf-8"
         parameters = JSON.stringify(parameters);
 	}
     return $.ajax({
-        "url": URL + url,
+        // non serve alcun tipo di concatenamento in quanto
+        // il browser, automaticamente, richiede il file allo stesso
+        // server che ci ha fornito la pagina
+        "url": url,
 		"data": parameters,
 		"type": method,   
 		"contentType": contentType, 
-        "dataType": "json",   // default      
-        "timeout": 5000,      // default 
-    });	
+        "dataType": "json",   
+        "timeout": 5000,      // default
+    });
 }
-
 
 function errore(jqXHR, text_status, string_error) {
     if (jqXHR.status == 0)
         alert("Connection Refused or Server timeout");
-	else if (jqXHR.status == 200)
+        // status significa che la risposta è arrivata correttamente
+        // ma è andato in errore in quanto il client non è riuscito a parsificare i dati
+	else if (jqXHR.status == 200)// 200 significa OK
         alert("Formato dei dati non corretto : " + jqXHR.responseText);
+        // un qualunque numero diverso da 200 significa errore, se il server
+        // rimanda in dietro un numero diverso da 200 mostriamo semplicemente
+        // l'errore che ci ritorna 
+    else if (jqXHR.status == 403) {  
+        window.location.href="login.html";
+    }    
     else
         alert("Server Error: " + jqXHR.status + " - " + jqXHR.responseText);
 }
